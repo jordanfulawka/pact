56c851874ab0e9f12cb26a944f4cd3bbd14e0615
jordanfulawka <98784255+jordanfulawka@users.noreply.github.com>
2026-06-30 15:44:41 -0400

    feat: add httpAuth middleware

---
+++ server/src/middlewares/httpAuth.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+
+function httpAuth(req: Request, res: Response, next: NextFunction) {
+  const token = req.headers.authorization?.split(' ')[1];
+
+  if (!token) {
+    return res.status(500).json({ error: 'Invalid token' });
+  }
+
+  const secret = process.env.JWT_SECRET;
+  if (!secret) {
+    return res.status(500).json({ error: 'No JWT secret' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, secret);
+    (req as any).user = decoded;
+  } catch (err) {
+    res.status(401).json({ error: 'Unauthroized' });
+  }
+
+  next();
+}
+
+export { httpAuth };
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import authRouter from './routes/auth';
+import pactRouter from './routes/pacts';
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRouter);
+app.use('/api/pacts', pactRouter);
 
 export default app;
