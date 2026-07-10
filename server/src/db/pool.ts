0874688d2e5ff575bbb8150987d96e54ce8d4ab8
jordanfulawka <98784255+jordanfulawka@users.noreply.github.com>
2026-07-10 18:20:58 -0400

    feat: setting database time to EST for consistency

--- server/src/db/pool.ts
+++ server/src/db/pool.ts
@@ -3,4 +3,9 @@ const { Pool } = pg;
 
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
+// RUN ALTER DATABASE COMMAND TO CHANGE DATABASE TIMEZONE WHEN DEPLOYING
+pool.on('connect', async (client) => {
+  await client.query("SET TIME ZONE 'America/New_York'");
+});
+
 export default pool;
